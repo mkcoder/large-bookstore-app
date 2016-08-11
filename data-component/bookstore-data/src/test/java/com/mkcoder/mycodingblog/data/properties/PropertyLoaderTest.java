c6029c12a35abf6d6f92ac10f13138b960513a8b
Muhammad K Khan <mk.muhammad.khan10@gmail.com>
2016-08-10 22:27:22 -0500

    update test code to reflect the goal

--- data-component/bookstore-data/src/test/java/com/mkcoder/mycodingblog/data/properties/PropertyLoaderTest.java
+++ data-component/bookstore-data/src/test/java/com/mkcoder/mycodingblog/data/properties/PropertyLoaderTest.java
@@ -1,8 +1,30 @@
 package com.mkcoder.mycodingblog.data.properties;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  *a simple test that checks if the PropertyLoader loads the actually file from the disk
  */
+@Component
 public class PropertyLoaderTest {
+
+    @Test
+    public void testIfPropertyFileLoadsCorrectValuesUsingSpringContext() {
+            String path = "application-context.xml";
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext(path);
+        PropertyLoader propertyLoader = (PropertyLoader) context.getBean("jdbc_bean");
+        assertEquals("abc123", propertyLoader.getJdbcPassword());
+        assertEquals("root", propertyLoader.getJdbcUsername());
+        assertEquals("null", propertyLoader.getJdbcUrl());
+
+    }
+
 
 }
